cbdaaf1eaffa4be301a4be17f02533c5dadf0342
corvaisier <julien.corvaisier.br@gmail.com>
2021-04-15 15:10:06 +0200

    add a sanitizer for clean data

--- app.js
+++ app.js
@@ -2,25 +2,25 @@ let express = require('express')
 let app = express()
 const fetch = require('isomorphic-fetch')
 const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+const {
+    JSDOM
+} = jsdom;
 
-app.get('/status', (req, res) => {
-    res.send('ok')
-})
-app.get('/baba', (req, res) => {
+let title = [];
+let size = [];
+let location = [];
+let price = [];
+let energy = [];
+let foundation = [];
+let textBody = [];
+let textFooter = [];
+//let img = [];
+
+function fetchData() {
+
     (async () => {
         let count = 1;
-        let title = [];
-        let size = [];
-        let location = [];
-        let price = [];
-        let energy = [];
-        let foundation = [];
-        let textBody = [];
-        let textFooter = [];
-        //let img = [];
-
-        for(let i = 0; i < 15; i++) {
+        for (let i = 0; i < 15; i++) {
             const response = await fetch('https://simply-home.herokuapp.com/house' + count + '.php');
             const text = await response.text();
             const dom = await new JSDOM(text);
@@ -33,11 +33,13 @@ app.get('/baba', (req, res) => {
             textBody.push(dom.window.document.getElementById("articleContent").textContent);
             textFooter.push(dom.window.document.getElementById("articleSubContent").textContent);
             //img.push(dom.window.document.getElementById("singleArticleImage").textContent);
-
             count++;
-        }
-        console.log(textBody);
-        console.log(textFooter);
-      })()
-    })
-app.listen(4200)
+        };
+
+        let sizeSanitized = size.map(x => x.replace('m²', '').replace('m', ''));
+        let locationSanitized = location.map(x => x.replace(/[0-9]/g, ''));
+        let priceSanitized = price.map(x => x.slice(0, -1));
+        let foundationSanitized = foundation.filter(x =>!isNaN(x));
+    })();
+};
+fetchData();
